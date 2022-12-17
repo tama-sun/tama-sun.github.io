77dc4dbd635a0aaaebb3780dc67c8cc41f12d845
tama <95291434+tama-sun@users.noreply.github.com>
2022-12-17 10:47:47 +0700

    Add files via upload

---
+++ rose.py
@@ -0,0 +1,10 @@
+import turtle
+t = turtle.Turtle()
+for i in range (180) :				
+				t.speed(0)
+				t.color('#CE7777')
+				t.circle(190 - i, 90)
+				t.left(90)
+				t.color('#2B3A55')
+				t.circle(190 - i, 90)
+				t.left(18)
